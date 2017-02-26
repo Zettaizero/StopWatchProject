76446875aef00f75aa55fe67874a495c1eb7959b
amitchell13 <amitchell13@SCW7MJ02ZGRN>
2017-02-26 17:35:31 -0500

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Press the enter key or type \"start\" or \"stop\" to run the stopwatch: ");
+                string input = Console.ReadLine();
+
+                if (input == "start" || String.IsNullOrWhiteSpace(input)) { StopWatch.Start(); continue; }
+                if (input == "stop") { StopWatch.Stop();
+                    Console.WriteLine();
+                    Console.WriteLine("Duration: " + StopWatch.CalculateInterval().ToString("ss") +" seconds."); }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication3/Watch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class StopWatch
+    {
+        private static bool _running;
+        private static DateTime _startTime;
+        private static DateTime _endTime;
+
+        public static void Start()
+        {
+            if (_running)
+                throw new InvalidOperationException("StopWatch is already running!");
+
+                _running = true;
+            _startTime = DateTime.Now;
+        }
+
+        public static void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("StopWatch is not running!");
+
+            _running = false;
+            _endTime = DateTime.Now;
+        }
+
+        public static TimeSpan CalculateInterval()
+        {
+            var duration = _endTime - _startTime;
+
+            return duration;
+        }
+    }
+}
